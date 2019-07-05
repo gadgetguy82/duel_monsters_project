000d1cbf51f9ad5bfa5e5715d960e8d78a71676e
Steve Gossip <stephengossip@gmail.com>
2019-07-05 21:12:26 +0100

    router.get for SHOW working in insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -7,7 +7,23 @@ const createRouter = function(collection) {
 
   router.get('/', (req, res) => {
     collection.find().toArray()
-    .then((docs) => res.json(docs));
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  }),
+
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection.findOne({ _id: ObjectID(id) })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error:err });
+    });
   })
   return router;
 };
