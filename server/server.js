29c71ba227171b88ce20ebcbce7cedf8b147a031
Robert Tam <rsh.tam@gmail.com>
2019-07-05 15:45:09 +0100

    Implement functions for server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const app = express();
+
+const parser = require('body-parser');
+app.use(parser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const create_router = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017', (error, client) => {
+  if (error) {
+    console.log(error);
+  }
+  const db = client.db('duel_monsters_game');
+  const cardsCollection = db.collection('cards');
+  const playerDeckCollection = db.collection('player_deck');
+  const cardsRouter = create_router(cardsCollection);
+  app.use('/api/duel_monsters_game', cardsRouter);
+});
+
+app.listen(3000, function() {
+  console.log(`Listening on port ${this.address().port}`);
+})
