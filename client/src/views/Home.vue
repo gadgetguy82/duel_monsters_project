7490a0f920d15b522373825e2ece5d7d155de1cd
Robert Tam <rsh.tam@gmail.com>
2019-07-05 21:47:41 +0100

    Add function to fetch api data

---
+++ client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+    <button v-on:click="fetchData">Update Database</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      allCards: []
+    }
+  }
+  methods: {
+    fetchData() {
+      fetch("https://db.ygoprodeck.com/api/v5/cardinfo.php")
+      .then(res => res.json()).then(cardData => this.allCards = cardData);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
