f5909c0ad2494c10db8e8b72e04c4128b5c382f3
Steve Gossip <stephengossip@gmail.com>
2019-07-06 17:07:36 +0100

    changed /Battlefield to /BattleField in import for router.js. There was a no compile error in the npm run serve before the fix

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home';
-import Battlefield from '@/views/Battlefield';
+import Battlefield from '@/views/BattleField';
 
 Vue.use(Router);
 
