3f67c1e23e35df9eb69adada919deb3de726f205
Robert Tam <rsh.tam@gmail.com>
2019-07-08 10:33:49 +0100

    Add socketio framework to project

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from '@/router.js';
+import VueSocketio from 'vue-socket.io';
+
+// Vue.use(VueSocketio, 'http://localhost:5000/');
 
 Vue.config.productionTip = false;
 
