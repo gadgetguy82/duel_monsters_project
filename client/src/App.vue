e2dafcd44007a246a99c61ad698c1ba2cd1a003e
Steve Gossip <stephengossip@gmail.com>
2019-07-08 09:58:50 +0100

    access to all normalMonsters on App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,13 +4,30 @@
       <router-link :to="{ name: 'home' }"> Home </router-link>
       <router-link :to="{ name: 'battlefield' }"> Battlefield </router-link>
     </nav>
-    <router-view id='view'></router-view>
+    <router-view id='view' :normCards="normalMonsterCards"></router-view>
   </div>
 </template>
 
 <script>
+import DBService from '@/services/db_service'
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return {
+      allDatabaseCards: [],
+      normalMonsterCards: []
+    }
+  },
+  mounted() {
+    DBService.getAllCards()
+    .then(cards => { this.allDatabaseCards = cards
+      this.allDatabaseCards.forEach((card) => {
+        if (card.type === "Normal Monster") {
+          this.normalMonsterCards.push(card)
+        }
+      });
+    });
+  }
 }
 </script>
 
