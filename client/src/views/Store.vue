ce67732d87dedb168efb311c3c18401a5a326a07
Katherine Kerr <katherinekerr@protonmail.com>
2019-07-10 15:12:18 +0100

    store displaying 100 normal cards

--- client/src/views/Store.vue
+++ client/src/views/Store.vue
@@ -4,7 +4,10 @@
     <div id="store-container">
       <h1>Duel Monsters Store</h1>
       <div class="store-cards-container">
-
+        <div class="store-card" v-for="(card) in normalStoreCards">
+          <p>{{card.name}}</p>
+          <button>Buy!</button>
+        </div>
       </div>
     </div>
 
@@ -14,13 +17,43 @@
 </template>
 
 <script>
+import DBService from '@/services/db_service'
+
 export default {
+  name: 'store',
+  data() {
+    return {
+      allStoreCards: [],
+      normalStoreCards: []
+    }
+  },
+  mounted() {
+    DBService.getAllCards()
+    .then(cards => { this.allStoreCards = cards
+      this.allStoreCards.forEach((card) => {
+        if ((card.type === "Normal Monster") && (this.normalStoreCards.length < 100)) {
+          this.normalStoreCards.push(card)
+        }
+      });
+    });
+  }
 }
 </script>
 
 <style lang="css" scoped>
-#store-container {
+/* #store-container {
   height: 809px;
 
+} */
+.store-cards-container {
+ display: flex;
+ flex-wrap: wrap;
+}
+
+.store-card {
+  background-color: rgba(255, 255, 255, 0.5);
+  border-width: 1px;
+  border-style: solid;
+  width: 100px;
 }
 </style>
