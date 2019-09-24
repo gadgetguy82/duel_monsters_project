fa606fcb63f54e2035abfda9fe0e14c62e5b15f8
Robert Tam <rsh.tam@gmail.com>
2019-09-24 12:16:05 +0100

    add temp boiler plate to extra monster zone

--- client/src/components/ExtraMonsterZone.vue
+++ client/src/components/ExtraMonsterZone.vue
@@ -1,5 +1,10 @@
 <template lang="html">
+  <div class="extra-monster-zone-container">
+    <h4>Extra Monster Zone</h4>
+    <div class="extra-monster-zone">
 
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +14,25 @@ export default {
 </script>
 
 <style lang="css" scoped>
+h4 {
+  margin: 0;
+  padding: 0 10px;
+  color: #FFFFFF;
+  text-shadow: 1px 1px 2px black, 0 0 2px black, 0 0 2px black;
+}
+
+.extra-monster-zone-container {
+  text-align: center;
+}
+
+.extra-monster-zone {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 5px;
+  width: 100px;
+  height: 145px;
+  background-image: url("../../public/img/card_back.png");
+  background-repeat: no-repeat;
+  background-size: 100px;
+}
 </style>
